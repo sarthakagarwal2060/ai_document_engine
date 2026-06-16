4a46ef18f71effe73ab745c132a41996fdd3c690
Aana-1025 <antara.24bcs10106@sst.scaler.com>
2026-06-17 01:37:05 +0530

    add search_with_citations to ChromaDB to fetch context metadata

--- ai_doc_engine/engine/rag_store.py
+++ ai_doc_engine/engine/rag_store.py
@@ -5,8 +5,18 @@ class DocVectorStore:
         self.client = chromadb.PersistentClient(path="./chroma_db")
         self.collection = self.client.get_or_create_collection(name="repo_docs")
 
-    def upsert_doc(self, doc_id, text, metadata):
+    def upsert_doc(self, doc_id, text, metadata=None, unit=None):
         """Insert or update a documentation chunk."""
+        if unit:
+            metadata = {
+                "unit_type": unit.unit_type,
+                "name": unit.name,
+                "file_path": doc_id,
+            }
+        
+        if metadata is None:
+            metadata = {}
+            
         self.collection.upsert(
             documents=[text],
             metadatas=[metadata],
@@ -20,3 +30,15 @@ class DocVectorStore:
             n_results=n_results
         )
         return results['documents'][0] if results['documents'] else []
+
+    def search_with_citations(self, query, n_results=3):
+        """Retrieve relevant documentation chunks AND their metadata for citations."""
+        results = self.collection.query(
+            query_texts=[query],
+            n_results=n_results
+        )
+        
+        if not results or not results['documents']:
+            return [], []
+            
+        return results['documents'][0], results['metadatas'][0]
